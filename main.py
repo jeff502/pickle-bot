4d1df83564e80c20ed5241486803eb066b097598
Jeff McElhiney <jeffmcelhineywork@gmail.com>
2024-02-13 00:31:41 -0600

    add pickle baller for each role

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 AUTHORIZATION_URI = "https://www.warcraftlogs.com/oauth/authorize"
-TOKEN_URI = "https://www.warcraftlogs.com/oauth/token" 
+TOKEN_URI = "https://www.warcraftlogs.com/oauth/token"
 PUBLIC_URL = "https://www.warcraftlogs.com/api/v2/client"
 
 QUERY = """
@@ -34,44 +34,74 @@ def main():
         access_token = read_data()
 
     player_dict = {}
-    code = input("Please enter the Warcraft report code found on the ending of the url (E.g. https://sod.warcraftlogs.com/reports/THIS_CODE_HERE): ")
+    dps_dict = {}
+    healer_dict = {}
+    tank_dict = {}
+    get_dict = {"dps": dps_dict, "tanks": tank_dict, "healers": healer_dict}
+
+    code = input(
+        "Please enter the Warcraft report code found on the ending of the url (E.g. https://sod.warcraftlogs.com/reports/THIS_CODE_HERE): "
+    )
 
     data = get_data(QUERY, code=code)
     formated_data = data["data"]["reportData"]["report"]["rankings"]["data"]
 
-    for item in formated_data[1::2]:     # Skipping the fightid, encounter, etc info
-            for _, role in item["roles"].items():
-                list_of_characters = role["characters"]
-                
-                for char in list_of_characters:
-                    name = char.get('name')
-                    rank_percent = char.get('rankPercent')
+    for item in formated_data[1::2]:  # Skipping the fightid, encounter, etc info
+        for role, info in item["roles"].items():
+            list_of_characters = info["characters"]
+            role_dict = get_dict[role]
+            for char in list_of_characters:
+                name = char.get("name")
+                rank_percent = char.get("rankPercent")
 
-                    if isinstance(rank_percent, str):
-                        rank_percent = 0
-                    if name and rank_percent:
-                        player_dict[name] = player_dict.get(name, 0) + int(rank_percent)
+                if isinstance(rank_percent, str):
+                    rank_percent = 0
+                if name and rank_percent:
+                    player_dict[name] = player_dict.get(name, 0) + int(rank_percent)
+                    role_dict[name] = role_dict.get(name, 0) + int(rank_percent)
+
+    char_dicts = [
+        ("the raid", player_dict),
+        ("healers", healer_dict),
+        ("dps", dps_dict),
+        ("tanks", tank_dict),
+    ]
+    for char_info, char_dict in char_dicts:
+        sorted_data = [
+            [k, v]
+            for k, v in sorted(
+                char_dict.items(), key=lambda item: item[1], reverse=True
+            )
+        ]
+        pickle_baller = sorted_data[0]
+        if len(sorted_data) > 1:
+            pickle_receiver = sorted_data[-1]
+        else:
+            pickle_receiver = None
+        print(
+            f"The pickle baller for {char_info} is {pickle_baller[0]}, with a total parse score of {pickle_baller[1]}!"
+        )
+        if pickle_receiver:
+            print(
+                f"The pickle reciever is {pickle_receiver[0]}, with a total measly parse score of {pickle_receiver[1]}!"
+            )
+        print(
+            "--------------------------------------------------------------------------------------------------"
+        )
 
 
-    sorted_data = [[k, v] for k, v in sorted(player_dict.items(), key=lambda item: item[1], reverse=True)]
-    pickle_baller = sorted_data[0]
-    pickle_receiver = sorted_data[-1]
-
-    print(f"The pickle baller is {pickle_baller[0]}, with a total parse score of {pickle_baller[1]}!")
-    print(f"The pickle reciever is {pickle_receiver[0]}, with a total measly parse score of {pickle_receiver[1]}!")
-
-
-    
 def get_access_token():
     response = get_token_response()
     if response.status_code != 200:
-        print(f"Incorrect response from server with status code: {response.status_code}.")
+        print(
+            f"Incorrect response from server with status code: {response.status_code}."
+        )
         return
-    
+
     response_json = response.json()
     access_token = response_json["access_token"]
     return access_token
-    
+
 
 def get_data(query, **kwargs):
     headers = get_headers()
@@ -90,7 +120,7 @@ def get_headers():
 def write_data(data):
     with open("access_token.txt", "w+") as file:
         file.write(data)
-    
+
 
 def read_data():
     text = None
@@ -98,14 +128,13 @@ def read_data():
         text = file.readlines()
     return text
 
+
 def get_token_response():
     data = {"grant_type": "client_credentials"}
     user_data = (CLIENT_ID, CLIENT_SECRET)
     with requests.Session() as sess:
         response = sess.post(TOKEN_URI, data=data, auth=user_data)
     return response
-
-
 
 
 if __name__ == "__main__":
